de67a6acd5c279965535b7b9907c9465788f813e
charitywanjiku <charity.wanjiku1@student.moringaschool.com>
2024-01-31 17:35:11 +0300

    Assignment Completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,30 @@
 // Code EyesOnMe Component Here
+
+import React from 'react';
+
+class EyesOnMe extends React.Component {
+  // Event handler for the focus event on the button
+  handleFocus = () => {
+    console.log('Good!');
+  };
+
+  // Event handler for the blur event on the button
+  handleBlur = () => {
+    console.log('Hey! Eyes on me!');
+  };
+
+  render() {
+    return (
+      <div>
+        {/* Button with onFocus and onBlur event listeners */}
+        <button onFocus={this.handleFocus} onBlur={this.handleBlur}>
+          Eyes on me
+        </button>
+      </div>
+    );
+  }
+}
+
+export default EyesOnMe;
+
+
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,19 @@
 // Code Keypad Component Here
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+import React from 'react';
+
+function Keypad() {
+  // Event handler for the change event on the input
+  const handleChange = () => {
+    console.log('Entering password...');
+  };
+
+  return (
+    <div>
+      {/* Input field with type 'password' and onChange event listener */}
+      <input type="password" onChange={handleChange} />
+    </div>
+  );
 }
 
 export default Keypad;
